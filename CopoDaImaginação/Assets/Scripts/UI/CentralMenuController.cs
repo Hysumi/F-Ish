f7b8f6ba5dd5a4527669c9ca65516ec380c7aae5
Gabriel Soares <venzoorkin@gmail.com>
2017-03-30 15:22:23 -0300

    Menus alterados, scripts base feitos, o menu funciona de uma maneira plug in and play, então é só copiar o canvas da cena Dialog, colar na cena a utilizar e deletar o desnecessario

---
+++ CopoDaImaginação/Assets/Scripts/UI/CentralMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralMenuController : MonoBehaviour
+{
+    //Evento de pause para poder ser registrado de qualquer lugar (por exemplo parar o gameplay sem setar timescale para 0)
+    public delegate void OnPauseButtonClicked();
+    public static OnPauseButtonClicked PauseButtonClick;
+
+    //Função ativada pelo botão de pause
+    public void FirePauseEvent()
+    {
+        //Da fire no evento, qualquer funçao ligada a ele é ativada
+        PauseButtonClick();
+    }
+
+}
---
+++ CopoDaImaginação/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    //Referencia pro gameObject no Editor
+    public GameObject pauseMenuContainer;
+
+    void Start()
+    {
+        //Inscreve a função no evento
+        CentralMenuController.PauseButtonClick += PauseButtonHandler;
+    }
+
+    //Verifica a condição do GameObject contendo as opções no menu de pause
+    void PauseButtonHandler()
+    {
+        //SE ativo, desativa. SENÃO, Ativa
+        if (pauseMenuContainer.activeInHierarchy)
+        {
+            pauseMenuContainer.SetActive(false);
+        }
+        else
+        {
+            pauseMenuContainer.SetActive(true);
+        }
+    }
+}
+
