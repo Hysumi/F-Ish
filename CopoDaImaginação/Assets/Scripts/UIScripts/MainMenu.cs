49d46c6f8ec524269eed5db1e6ea0c59d82a15c7
Gabriel Soares <venzoorkin@gmail.com>
2017-04-06 14:30:50 -0300

    UI inicial, Controladores, divisão das pastas de UI.

---
+++ CopoDaImaginação/Assets/Scripts/UIScripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    MenuEvents menuEvents;
+    void Start()
+    {
+        menuEvents = GetComponentInParent<MenuEvents>();
+    }
+    public void ExitButton()
+    {
+        menuEvents.Exit();
+    }
+    public void RankingButton()
+    {
+        menuEvents.Ranking();
+    }
+    public void StartButton()
+    {
+        menuEvents.Exit();
+    }
+}
---
+++ CopoDaImaginação/Assets/Scripts/UIScripts/MenuEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEvents : MonoBehaviour
+{
+    public delegate void ExitCall();
+    public static event ExitCall ExitButtonEvent;
+
+    public delegate void RankingCall();
+    public static event RankingCall RankingButtonEvent;
+
+    public delegate void StartCall();
+    public static event StartCall StartButtonEvent;
+
+    internal void Exit()
+    {
+        ExitButtonEvent();
+    }
+    internal void Ranking()
+    {
+        RankingButtonEvent();
+    }
+    internal void Start()
+    {
+        StartButtonEvent();
+    }
+
+}
